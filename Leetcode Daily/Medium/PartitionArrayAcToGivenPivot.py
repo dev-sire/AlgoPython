1af68deed9e78a8b1aaf7ab33281568aa6c28a4f
dev-sire <shahidamaan2019@gmail.com>
2025-03-10 09:57:10 +0500

    added Count of Substring containing

---
+++ Leetcode Daily/Medium/PartitionArrayAcToGivenPivot.py
@@ -0,0 +1,7 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        return(
+            [num for num in nums if num < pivot] + 
+            [num for num in nums if num == pivot] +
+            [num for num in nums if num > pivot]
+        )
