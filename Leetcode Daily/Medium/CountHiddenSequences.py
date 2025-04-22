924e867c5410879fbb9879db4da3ee84ee2d2de5
dev-sire <shahidamaan2019@gmail.com>
2025-04-22 17:21:43 +0500

    added Count Num Of Ideal Arrays

---
+++ Leetcode Daily/Medium/CountHiddenSequences.py
@@ -0,0 +1,11 @@
+import itertools
+
+class Solution:
+  def numberOfArrays(
+      self,
+      differences: list[int],
+      lower: int,
+      upper: int,
+  ) -> int:
+    prefix = list(itertools.accumulate(differences, initial=0))
+    return max(0, (upper - lower) - (max(prefix) - min(prefix)) + 1)
