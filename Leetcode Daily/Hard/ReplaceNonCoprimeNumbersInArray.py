0363a1baa632952d303b9721b18c7f05507bb5f9
dev-sire <shahidamaan2019@gmail.com>
2025-09-16 23:05:15 +0500

    added replace non coprime numbers in array

---
+++ Leetcode Daily/Hard/ReplaceNonCoprimeNumbersInArray.py
@@ -0,0 +1,12 @@
+import math
+
+class Solution:
+  def replaceNonCoprimes(self, nums: list[int]) -> list[int]:
+    ans = []
+
+    for num in nums:
+      while ans and math.gcd(ans[-1], num) > 1:
+        num = math.lcm(ans.pop(), num)
+      ans.append(num)
+
+    return ans
