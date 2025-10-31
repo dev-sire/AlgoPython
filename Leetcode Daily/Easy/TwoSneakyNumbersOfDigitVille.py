de89fb7b9e9b4f4dad256b248d12f3cb5ce8fe51
dev-sire <shahidamaan2019@gmail.com>
2025-10-31 21:06:46 +0500

    added two sneaky numbers of digitville

---
+++ Leetcode Daily/Easy/TwoSneakyNumbersOfDigitVille.py
@@ -0,0 +1,6 @@
+import collections
+
+class Solution:
+  def getSneakyNumbers(self, nums: list[int]) -> list[int]:
+    return [num for num, freq in collections.Counter(nums).items()
+            if freq == 2]
