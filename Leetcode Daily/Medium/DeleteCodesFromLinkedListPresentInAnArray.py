5bec61faf53c2679864811f1707530dc8717b94d
dev-sire <shahidamaan2019@gmail.com>
2025-11-04 10:47:03 +0500

    added find x sum of k long subarrays I

---
+++ Leetcode Daily/Medium/DeleteCodesFromLinkedListPresentInAnArray.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+  def modifiedList(
+      self,
+      nums: list[int],
+      head: ListNode | None,
+  ) -> ListNode | None:
+    dummy = ListNode(0, head)
+    numsSet = set(nums)
+
+    curr = dummy
+    while curr.next:
+      if curr.next.val in numsSet:
+        curr.next = curr.next.next
+      else:
+        curr = curr.next
+
+    return dummy.next
+        
